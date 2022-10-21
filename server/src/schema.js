ed8fb1a9bc3bb44af85175068157fb9957d917b0
Pavlo Sokolov <pavlos@rabota.ua>
2022-10-21 11:20:04 +0300

    I Basics + II Resolvers

--- server/src/schema.js
+++ server/src/schema.js
@@ -4,8 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
-    "Fetch a specific track, provided a track's ID"
-    track(id: ID!): Track!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -21,12 +20,20 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
-    "The track's complete description, can be in markdown format"
+    "The tracks's complete description, can be in Markdown format"
     description: String
     "The number of times a track has been viewed"
     numberOfViews: Int
     "The track's complete array of Modules"
     modules: [Module!]!
+  }
+
+  type Module {
+    id: ID!
+    "The Module's title"
+    title: String!
+    "The Module's length in minutes"
+    length: Int
   }
 
   "Author of a complete Track or a Module"
@@ -36,15 +43,6 @@ const typeDefs = gql`
     name: String!
     "Author's profile picture"
     photo: String
-  }
-
-  "A Module is a single unit of teaching. Multiple Modules compose a Track"
-  type Module {
-    id: ID!
-    "The module's title"
-    title: String!
-    "The module's length in minutes"
-    length: Int
   }
 `;
 
